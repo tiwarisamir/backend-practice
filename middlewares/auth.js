f423f91bd07012055ec8bc324418436618554e2c
Samir Tiwari <samirtiwari2061@gmail.com>
2024-04-25 18:30:06 +0545

    fix bug

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 
 export const isAuthenticated = async (req, res, next) => {
   const { token } = req.cookies;
+  console.log(token);
 
   if (!token)
     return res.status(404).json({
