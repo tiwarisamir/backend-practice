3777e1625e1f28f3fd34d45acfa109242a0e15e4
Samir Tiwari <samirtiwari2061@gmail.com>
2024-04-27 14:13:34 +0545

    fix bug

--- utils/features.js
+++ utils/features.js
@@ -8,8 +8,10 @@ export const sendCookie = (user, res, message, statusCode = 200) => {
     .cookie("token", token, {
       httpOnly: true,
       maxAge: 15 * 60 * 1000,
-      sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
-      secure: process.env.NODE_ENV === "Development" ? true : false,
+      // sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
+      // secure: process.env.NODE_ENV === "Development" ? true : false,
+      sameSite: "none",
+      secure: false,
     })
     .json({
       success: true,
